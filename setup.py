923139c90dac5cf46c7887d6fc267dd47cbe1eeb
Siva Prasanna <shivachatti66@gmail.com>
2025-11-17 18:03:44 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+
+def get_requirements(path):
+    '''
+        This function will return the list of
+        requirements.
+    '''
+    requirements = []
+    with open(path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        
+        if "-e ." in requirements:
+            requirements.remove('-e .')
+    return requirements
+
+setup(
+    name='ML Project',
+    version='0.0.1',
+    author='Siva Prasanna',
+    author_email='shivachatti190@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+    
+)
